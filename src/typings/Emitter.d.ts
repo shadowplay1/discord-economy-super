c37f08486c9004d329b00c9d4f7fdbdaa9d17ce3
shadowplay1 <43500997+shadowplay1@users.noreply.github.com>
2021-08-08 01:05:09 +0300

    Delete src/typings directory

--- src/typings/Emitter.d.ts
+++
@@ -1,8 +0,0 @@
-/**
-* Simple Economy event emitter with only important emitter methods.
-*/
-export default class Emitter {
-    on(event: String, fn: Function): void
-    once(event: String, fn: Function): void
-    emit(event: String, data: any): void
-}
